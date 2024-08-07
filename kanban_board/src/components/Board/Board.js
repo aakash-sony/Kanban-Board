a844aabb07eae06e147a9706ca5bc59ae040135f
Akash <aakashsoni8756@gmail.com>
2024-08-08 02:09:04 +0530

    task saved in local storage

--- kanban_board/src/components/Board/Board.js
+++ kanban_board/src/components/Board/Board.js
@@ -2,70 +2,72 @@ import React, { useState, useEffect, useReducer, createContext } from "react";
 import NewTask from "./NewTask/NewTask";
 import BoardLanes from "./BoardLanes";
 import "./Board.css";
+
 const stagesData = [
     { name: "To Do", id: 1 },
     { name: "In Progress", id: 2 },
     { name: "Peer Review", id: 3 },
     { name: "Done", id: 4 },
 ];
-const taskData = [
-];
+
+const initialTasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
 export const BoardContext = createContext({});
 
 function reducer(state, action) {
+    let updatedTasks;
     switch (action.type) {
         case "ON_DROP":
             const droppedTask = action.payload;
-            const updatedTasks = state.map((task) => {
+            updatedTasks = state.map((task) => {
                 if (task.id === droppedTask.id) {
                     return droppedTask;
                 }
                 return task;
             });
+            localStorage.setItem("tasks", JSON.stringify(updatedTasks));
             return updatedTasks;
         case "LOAD_DATA":
             return action.payload;
         case "ADD_NEW":
-            return [...state, action.payload];
-        case "ON_DELETE":
-            return state.filter((task) => task.id !== action.payload);
+            updatedTasks = [...state, action.payload];
+            localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+            return updatedTasks;
         default:
             return state;
     }
 }
+
 function Board() {
-    const [taskState, dispatch] = useReducer(reducer, taskData);
+    const [taskState, dispatch] = useReducer(reducer, initialTasks);
     const [stages, setStage] = useState(stagesData);
+    const [searchQuery, setSearchQuery] = useState("");
 
     useEffect(() => {
-        dispatch({ type: "LOAD_DATA", payload: taskState });
-    }, [taskState, stages]);
+        const savedTasks = JSON.parse(localStorage.getItem("tasks"));
+        if (savedTasks) {
+            dispatch({ type: "LOAD_DATA", payload: savedTasks });
+        }
+    }, []);
 
     const onDragStartHandler = (event, taskId, stageId) => {
-        var data = {
-            taskId: taskId,
-            stageId: stageId
-        };
+        var data = { taskId: taskId, stageId: stageId };
         event.dataTransfer.setData("text/plain", JSON.stringify(data));
         event.dataTransfer.effectAllowed = "move";
-        //event.dataTransfer.setData("taskName", taskName);
     };
 
     const onTaskContainerDragStartHandler = (event, laneId) => {
         let fromBox = JSON.stringify({ laneId: laneId });
         event.dataTransfer.setData("laneId", fromBox);
     };
+
     const onTaskContainerDragOverHandler = (event) => {
         if (event.dataTransfer && event.dataTransfer.types[0] === "text/plain") {
             event.preventDefault();
         }
     };
 
-    const onTaskContainerDropHandler = (event, droppedLaneId) => {
-        // let fromLane: any = JSON.parse(event.dataTransfer.getData("laneId"));
-        // swapStages(fromLane.laneId, droppedLaneId)
-    };
+    const onTaskContainerDropHandler = (event, laneId) => { };
 
     const onDragOverHandler = (event) => {
         if (event.dataTransfer && event.dataTransfer.types[0] === "text/plain") {
@@ -87,22 +89,19 @@ function Board() {
         dispatch({ type: "ADD_NEW", payload: dataFromChild });
     };
 
-    const onUpdatingTask = (dataFromChild) => {
-        console.log(dataFromChild);
-        dispatch({ type: "ON_DROP", payload: dataFromChild });
+    const handleSearch = (query) => {
+        setSearchQuery(query);
     };
 
-    const onDeletingTask = (taskId) => {
-        dispatch({ type: "ON_DELETE", payload: taskId });
-    };
+    const filteredTasks = taskState.filter(task =>
+        task.title.toLowerCase().includes(searchQuery.toLowerCase())
+    );
 
     const ContextData = {
         taskState,
         onDragStartHandler,
         onDragOverHandler,
         onDropHandler,
-        onUpdatingTask,
-        onDeletingTask,
         onTaskContainerDragStartHandler,
         onTaskContainerDropHandler,
         onTaskContainerDragOverHandler
@@ -112,14 +111,14 @@ function Board() {
         <div className="container-fluid pt-3">
             <div className="row">
                 <div className="col-12">
-                    <NewTask addNewTask={onAddingNewTask} />
+                    <NewTask addNewTask={onAddingNewTask} handleSearch={handleSearch} searchQuery={searchQuery} />
                 </div>
             </div>
             <hr />
-            <div className="row">
+            <div className="row-12">
                 <div className="col-12">
                     <BoardContext.Provider value={ContextData}>
-                        <BoardLanes stages={stages}></BoardLanes>
+                        <BoardLanes stages={stages} tasks={filteredTasks}></BoardLanes>
                     </BoardContext.Provider>
                 </div>
             </div>
